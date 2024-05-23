aa1adbc59325565577604c080dd3ef902549ca2f
Filippo Illuminati <illuminati.filippo9@gmail.com>
2024-05-23 21:38:08 +0200

    First button and checkout component implementation

--- src/app/cart-page/cart-page.component.spec.ts
+++ src/app/cart-page/cart-page.component.spec.ts
@@ -1,16 +1,16 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { EditPopupComponent } from './edit-popup.component';
+import { CartPageComponent } from './cart-page.component';
 
-describe('EditPopupComponent', () => {
-  let component: EditPopupComponent;
-  let fixture: ComponentFixture<EditPopupComponent>;
+describe('CartPageComponent', () => {
+  let component: CartPageComponent;
+  let fixture: ComponentFixture<CartPageComponent>;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [EditPopupComponent]
+      declarations: [CartPageComponent]
     });
-    fixture = TestBed.createComponent(EditPopupComponent);
+    fixture = TestBed.createComponent(CartPageComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
---
+++ src/app/cart-page/cart-page.component.ts
@@ -0,0 +1,61 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cart-page',
+  templateUrl: './cart-page.component.html',
+  styleUrls: ['./cart-page.component.scss']
+})
+export class CartPageComponent {
+
+  selectedPaymentMethod: PaymentMethod|any;
+
+  selectPaymentMethod(type: PaymentMethod) {
+
+    this.selectedPaymentMethod=type;
+
+  }
+
+  paymentMethodSelected() {
+
+    console.log(this.selectedPaymentMethod)
+
+  }
+
+  formatCardNumber(event:any) {
+    // Rimuovi tutti i caratteri non numerici
+    let cardNumber = event.target.value.replace(/\D/g, '');
+    
+    // Formatta il numero con trattini ogni 4 cifre
+    cardNumber = cardNumber.replace(/(\d{4})(?=\d)/g, '$1-');
+    
+    // Aggiorna il valore nell'input
+    event.target.value = cardNumber;
+  }
+
+}
+
+export enum PaymentMethod {
+
+    VISA,
+
+    MASTERCARD,
+
+    GPAY,
+
+    APPPAY,
+
+    PAYPAL,
+
+    AMAZONPAY
+
+  }
+
+  export class Cvv {
+    numero: string = '';
+  
+    limitaInput(event: any) {
+      if (event.target.value.length > 3) {
+        event.target.value = event.target.value.slice(0, 3);
+      }
+    }
+  }
